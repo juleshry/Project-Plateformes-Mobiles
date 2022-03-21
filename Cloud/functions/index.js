ab6304911e8fa7df7d49a1e397817fb49292d5ba
Toué Stéphane <stephanet88@gmail.com>
2022-03-21 13:54:40 -0400

    adding firestore database --needs fixing

---
+++ Cloud/functions/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.addUsertoFirestore = functions.auth.user().onCreate((user) => {
+  const usersRef = admin.firestore().collection("users");
+  return usersRef.doc(user.uid).set({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    displayName: user.displayName,
+    // password ?
+  });
+});
